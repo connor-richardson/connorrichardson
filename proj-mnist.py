3404f8de34a0166566d3b406563c8119fa587f43
connor-richardson <154557076+connor-richardson@users.noreply.github.com>
2024-01-18 17:20:32 -0800

    Add files via upload

---
+++ proj-mnist.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import numpy as np 
+import mnist 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+
+#Load MNIST datasets
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
+
+#Normalize data into [-0.5, 0.5]
+train_images = (train_images / 255) - 0.5
+test_images = (test_images / 255) - 0.5
+
+#Flatten images
+train_images = train_images.reshape((-1, 784))
+test_images = test_images.reshape((-1, 784))
+
+print(train_images.shape)
+print(test_images.shape)
+
+
+
+# from tensorflow.keras.models import Sequential
+model = Sequential([
+  Dense(64, activation='relu', input_shape=(784,)),
+  Dense(64, activation='relu'),
+  Dense(10, activation='softmax'),
+])
+
+model.compile(
+  optimizer='adam',
+  loss='categorical_crossentropy',
+  metrics=['accuracy'],
+)
+
+
+model.fit(
+  train_images,
+  to_categorical(train_labels),
+  epochs=5,
+  batch_size=32,
+)
